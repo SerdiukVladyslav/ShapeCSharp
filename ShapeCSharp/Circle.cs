1371837149ebfc21c08eecc89d4decb289124eea
Vladyslav <vlad.serdyuk.3456@gmail.com>
2023-11-30 22:47:35 +0200

    #Task1 Створення классу Circle.
    Клас Circle, який успадковується від абстрактного класу Shape та перевизначає методи класу Shape. Цей клас створює фігуру коло.

--- ShapeCSharp/Circle.cs
+++ ShapeCSharp/Circle.cs
@@ -4,6 +4,30 @@ namespace ShapeCSharp
 {
     class Circle : Shape
     {
-        
+        private double centerX, centerY, radius;
+
+        public Circle()
+        {
+            centerX = 0;
+            centerY = 0;
+            radius = 0;
+        }
+
+        public Circle(double centerX, double centerY, double radius)
+        {
+            this.centerX = centerX;
+            this.centerY = centerY;
+            this.radius = radius;
+        }
+
+        public double Radius
+        {
+            get { return radius; }
+        }
+
+        public override void Show()
+        {
+            Console.WriteLine("Коло з центром ({0},{1}) i радiусом {2}", centerX, centerY, radius);
+        }
     }
 }
